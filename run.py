9b150b7ccf2adb9ccd5181718ffc25c7d3c462ee
Jeanbaptiste <27924537+Jean7667@users.noreply.github.com>
2024-02-16 11:21:44 +0000

    fct add to check the format of user input

--- run.py
+++ run.py
@@ -16,18 +16,44 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+#check access to data API connection
 """ sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 print(data)
  """
-#fct get sales data from the user
 
+#fct get sales data from the user
 def get_sales_data():
     print("please input sales data")
     print("expected format 6 numbers separated by commas")
     print("Example: 1,11,22,24,1,21\n")  
+    
     data_str = input("enter your numbers here: ")
-    print(f"you gave the following numbers {data_str}")
+    
+    #print(f"you gave the following numbers {data_str}")
+
+    sales_data = data_str.split(",")
+    #call fct to check format
+    validate_data(sales_data)
+    #print(sales_data)
+
+def validate_data(values):
+    """
+    check format of data
+    converts all strings values into int 
+    raises value erroe if strings cannont be converted
+    """
+    #print(values)
+    try:
+        if len(values) != 6:
+            raise ValueError(  
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+#e shorthand error python
+
 #call fct
 get_sales_data()
     
